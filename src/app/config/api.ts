7b3d19469ac3e8a379de64db73079fdf1accb997
Alex Arriaga <alexzander.arriaga@base22.com>
2019-06-19 20:56:02 -0500

    Agregar ngx-webstorage

---
+++ src/app/config/api.ts
@@ -0,0 +1,5 @@
+export const API = {
+  AUTH_SERVICES_BASE_URL: 'http://rentals-api.webtraining.fun/api', // 'http://127.0.0.1:8000/api',
+  DATA_SERVICES_BASE_URL: 'http://rentals-api.webtraining.fun/api', // 'http://127.0.0.1:8000/api',
+  IMAGES_BASE_URL: 'http://rentals-api.webtraining.fun/images'// 'http://127.0.0.1:8000/images'
+};

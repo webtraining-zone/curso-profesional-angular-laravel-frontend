e108d11a11f9e27d25e69ebd96c4f1e67a4df6f3
Alex Arriaga <alexzander.arriaga@base22.com>
2019-06-19 20:44:50 -0500

    Agregar ejemplo de routing básico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './public/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: '/home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full',
+    // canActivate: [PublicGuard]
+  },
+  // {
+  //   path: 'auth-home',
+  //   component: AuthHomeComponent,
+  //   data: {name: 'AuthHome'},
+  //   canActivate: [AuthGuard]
+  // },
+  // {path: 'projects/:id', component: ProjectDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { HeaderComponent } from './common/header/header.component';
+import { LoginFormComponent } from './public/login/login-form/login-form.component';
+import { HomeComponent } from './public/home/home.component';
+import { FullBannerComponent } from './public/home/full-banner/full-banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginFormComponent,
+    HomeComponent,
+    FullBannerComponent
   ],
   imports: [
     BrowserModule,

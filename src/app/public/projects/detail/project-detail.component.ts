d8922f50e8c0d2640a49b4f4270402a257da97e1
Alex Arriaga <alexzander.arriaga@base22.com>
2019-06-20 09:06:36 -0500

    Agregar soporte para detalle de proyecto

---
+++ src/app/public/projects/detail/project-detail.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {Project} from '../models/project.model';
+import {ActivatedRoute} from '@angular/router';
+import {ProjectsService} from '../services/projects.service';
+import {Location} from '@angular/common';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './project-detail.component.html',
+  styleUrls: ['./project-detail.component.scss'],
+})
+export class ProjectDetailComponent implements OnInit {
+  project: Project;
+
+  constructor(
+    private route: ActivatedRoute,
+    private projectsService: ProjectsService,
+    private location: Location) {
+  }
+
+  ngOnInit() {
+    this.getProject();
+  }
+
+  getProject() {
+    // Get the "id" from the route
+    const slug = parseInt(this.route.snapshot.paramMap.get('slug'), 10);
+    this.projectsService.getProject(slug).
+      subscribe(
+        project => this.project = project,
+        error => console.error(error),
+      );
+    // this.vehicle = this._vehiclesService.getProject(id);
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+}

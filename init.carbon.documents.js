09e14f554a6626e2430e48a493d9b9a06ba1e293
Alejandro Serrano <alexserrano63@gmail.com>
2019-07-06 02:06:10 -0500

    Fix init script for carbon documents

--- init.carbon.documents.js
+++ init.carbon.documents.js
@@ -3,25 +3,25 @@ let {CarbonLDP} = require('carbonldp');
 const BASE_CARBON_URL = 'http://localhost:8083';
 let carbon = new CarbonLDP(BASE_CARBON_URL);
 
-carbon.documents.$exists('projects').then((exist) => {
+carbon.documents.$exists('projects/').then((exist) => {
   if (!exist) {
     carbon.documents.$create('/', {}, 'projects');
   }
 }).catch(e => console.log(e));
 
-carbon.documents.$exists('en').then((exist) => {
+carbon.documents.$exists('en/').then((exist) => {
   if (!exist) {
     carbon.documents.$create('/', {}, 'en');
   }
 }).catch(e => console.log(e));
 
-carbon.documents.$exists('es').then((exist) => {
+carbon.documents.$exists('es/').then((exist) => {
   if (!exist) {
     carbon.documents.$create('/', {}, 'es');
   }
 }).catch(e => console.log(e));
 
-carbon.documents.$exists('users').then((exist) => {
+carbon.documents.$exists('users/').then((exist) => {
   if (!exist) {
     carbon.documents.$create('/', {}, 'users');
   }

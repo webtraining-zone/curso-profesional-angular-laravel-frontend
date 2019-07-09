ba6d3a5a1827b65d83279a3fd2a798cddd477656
Alejandro Serrano <alexserrano63@gmail.com>
2019-07-09 01:29:14 -0500

    Arreglar tipos en carbonldp esquemas

--- src/app/config/carbonLDP/carbon-init.ts
+++ src/app/config/carbonLDP/carbon-init.ts
@@ -5,8 +5,8 @@ const BASE_CARBON_URL = 'http://localhost:8083';
 const carbon = new CarbonLDP(BASE_CARBON_URL);
 
 carbon.extendObjectSchema(BASE_CARBON_LDP_SCHEMA);
-carbon.extendObjectSchema('Comment', COMMENTS_SCHEMA);
-carbon.extendObjectSchema('Project', PROJECT_SCHEMA);
-carbon.extendObjectSchema('User', USER_SCHEMA);
+carbon.extendObjectSchema('wt:Comment', COMMENTS_SCHEMA);
+carbon.extendObjectSchema('wt:Project', PROJECT_SCHEMA);
+carbon.extendObjectSchema('wt:User', USER_SCHEMA);
 
 export default carbon;

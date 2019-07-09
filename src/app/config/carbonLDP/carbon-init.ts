7419e742b7a258a4916937683598593e3aa5b21f
Alejandro Serrano <alexserrano63@gmail.com>
2019-07-08 23:52:58 -0500

    Add base schema

--- src/app/config/carbonLDP/carbon-init.ts
+++ src/app/config/carbonLDP/carbon-init.ts
@@ -1,8 +1,10 @@
 import {CarbonLDP} from 'carbonldp';
-import {PROJECT_SCHEMA, COMMENTS_SCHEMA, USER_SCHEMA} from './schemas';
+import {BASE_CARBON_LDP_SCHEMA, COMMENTS_SCHEMA, PROJECT_SCHEMA, USER_SCHEMA} from './schemas';
+
 const BASE_CARBON_URL = 'http://localhost:8083';
 const carbon = new CarbonLDP(BASE_CARBON_URL);
 
+carbon.extendObjectSchema(BASE_CARBON_LDP_SCHEMA);
 carbon.extendObjectSchema('Comment', COMMENTS_SCHEMA);
 carbon.extendObjectSchema('Project', PROJECT_SCHEMA);
 carbon.extendObjectSchema('User', USER_SCHEMA);
